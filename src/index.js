bcc99079a31a8e348cc307ae67eb4993faffc93c
Ishika Goyadi <ishikag@pool-100-0-156-228.bstnma.east.verizon.net>
2025-06-10 12:00:18 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import 'dotenv/config'
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(` Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db Connection failed !!! ",err)
+})
 
 
 /** 
